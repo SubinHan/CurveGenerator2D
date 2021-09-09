eb46726642d1c3aeecb8193dcf2eee86448d3bd4
5ubin <hanidiot@gmail.com>
2021-09-09 18:04:28 +0900

    removed YAGNI things and simplified

--- CurveGenerator2D.h
+++ CurveGenerator2D.h
@@ -1,16 +1,35 @@
 #pragma once
 #include "ICurveDrawer.h"
 
+struct Point2D 
+{
+    int x, y;
+};
+
+struct ControlPoints 
+{
+    Point2D p1, p2, p3, p4;
+};
+
+struct Configs 
+{
+
+};
+
 class CurveGenerator2D
 {
 public:
-    CurveGenerator2D();
+    CurveGenerator2D(int x, int y) : m_nScaleX(x), m_nScaleY(y) 
+    {
+        m_configs = Configs{};
+    }
     ~CurveGenerator2D();
-    void AddCurveDrawer(ICurveDrawer drawer);
-    void SetScale(int x, int y);
-    void Draw();
+    void ChangeScale(int x, int y);
+    Point2D* GetCurve(ControlPoints points);
+    Point2D Plot(ControlPoints points, float t);
 
 private:
     int m_nScaleX;
     int m_nScaleY;
+    Configs m_configs;
 };
--- ICurveDrawer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Color.h"
-
-class ICurveDrawer 
-{
-public:
-    virtual void Draw(float x, float y, Color color = Color(0, 0, 0, 100));
-};
